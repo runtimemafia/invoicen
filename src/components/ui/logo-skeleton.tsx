d4c76f43b7c2146e3ae7d1bd41010d68f62e8968
Prithvi <prithvirajindulkar2003@gmail.com>
2024-11-04 17:07:43 +0530

    reusable skeleton

--- src/components/ui/logo-skeleton.tsx
+++
@@ -1,9 +0,0 @@
-import { cn } from "@/lib/utils";
-
-const Skeleton = () => {
-  return (
-    <div className={cn("animate-pulse bg-muted h-[60px] w-[60px] rounded-md")}></div>
-  );
-};
-
-export { Skeleton };
